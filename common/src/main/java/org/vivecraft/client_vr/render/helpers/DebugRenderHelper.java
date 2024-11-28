b15a91ac511e64ad0e4b2fbb8c0f7d80ce3e3c11
fayer3 <bayer.florian@aon.at>
2024-11-28 23:12:49 +0100

    change debug render helper statics to CAPS

--- common/src/main/java/org/vivecraft/client_vr/render/helpers/DebugRenderHelper.java
+++ common/src/main/java/org/vivecraft/client_vr/render/helpers/DebugRenderHelper.java
@@ -22,29 +22,29 @@ import java.util.List;
 
 public class DebugRenderHelper {
 
-    private static final ClientDataHolderVR dataHolder = ClientDataHolderVR.getInstance();
-    private static final Minecraft mc = Minecraft.getInstance();
+    private static final ClientDataHolderVR DATA_HOLDER = ClientDataHolderVR.getInstance();
+    private static final Minecraft MC = Minecraft.getInstance();
 
-    private static final Vector3f red = new Vector3f(1F,0F,0F);
-    private static final Vector3f green = new Vector3f(0F,1F,0F);
-    private static final Vector3f blue = new Vector3f(0F,0F,1F);
+    private static final Vector3f RED = new Vector3f(1F,0F,0F);
+    private static final Vector3f GREEN = new Vector3f(0F,1F,0F);
+    private static final Vector3f BLUE = new Vector3f(0F,0F,1F);
 
     public static void renderDebug(PoseStack poseStack, float partialTick) {
-        if (dataHolder.vrSettings.renderDeviceAxes) {
-            renderDeviceAxes(poseStack, dataHolder.vrPlayer.vrdata_world_render);
+        if (DATA_HOLDER.vrSettings.renderDeviceAxes) {
+            renderDeviceAxes(poseStack, DATA_HOLDER.vrPlayer.vrdata_world_render);
         }
-        if (dataHolder.vrSettings.renderVrPlayerAxes) {
+        if (DATA_HOLDER.vrSettings.renderVrPlayerAxes) {
             renderPlayerAxes(poseStack, partialTick);
         }
     }
 
     public static void renderPlayerAxes(PoseStack poseStack, float partialTick) {
-        if (mc.player != null) {
+        if (MC.player != null) {
             BufferBuilder bufferbuilder = null;
             Vec3 camPos = RenderHelper
-                .getSmoothCameraPosition(dataHolder.currentPass, dataHolder.vrPlayer.getVRDataWorld());
+                .getSmoothCameraPosition(DATA_HOLDER.currentPass, DATA_HOLDER.vrPlayer.getVRDataWorld());
 
-            for(Player p : mc.player.level().players()) {
+            for(Player p : MC.player.level().players()) {
                 if (VRPlayersClient.getInstance().isVRPlayer(p)) {
                     VRPlayersClient.RotInfo info = VRPlayersClient.getInstance().getRotationsForPlayer(p.getUUID());
 
@@ -55,11 +55,11 @@ public class DebugRenderHelper {
                     }
 
                     Vector3f playerPos = p.getPosition(partialTick).subtract(camPos).toVector3f();
-                    if (p == mc.player) {
-                        playerPos = ((GameRendererExtension)mc.gameRenderer).vivecraft$getRvePos(partialTick).subtract(camPos).toVector3f();
+                    if (p == MC.player) {
+                        playerPos = ((GameRendererExtension) MC.gameRenderer).vivecraft$getRvePos(partialTick).subtract(camPos).toVector3f();
                     }
 
-                    if (p != mc.player || dataHolder.currentPass == RenderPass.THIRD) {
+                    if (p != MC.player || DATA_HOLDER.currentPass == RenderPass.THIRD) {
                         addAxes(poseStack, bufferbuilder, playerPos, info.headPos, info.headRot, info.headQuat);
                     }
                     if (!info.seated) {
@@ -86,24 +86,24 @@ public class DebugRenderHelper {
 
         list.add(data.c2);
 
-        if (dataHolder.currentPass == RenderPass.THIRD) {
+        if (DATA_HOLDER.currentPass == RenderPass.THIRD) {
             list.add(data.hmd);
         }
 
-        if (dataHolder.cameraTracker.isVisible()) {
+        if (DATA_HOLDER.cameraTracker.isVisible()) {
             list.add(data.cam);
         }
 
-        if (mc.player != null && TelescopeTracker.isTelescope(mc.player.getMainHandItem()) && TelescopeTracker.isViewing(0)) {
+        if (MC.player != null && TelescopeTracker.isTelescope(MC.player.getMainHandItem()) && TelescopeTracker.isViewing(0)) {
             list.add(data.t0);
         } else {
-            list.add(mc.player != null && mc.player.isShiftKeyDown() ? data.h0 : data.c0);
+            list.add(MC.player != null && MC.player.isShiftKeyDown() ? data.h0 : data.c0);
         }
 
-        if (mc.player != null && TelescopeTracker.isTelescope(mc.player.getOffhandItem()) && TelescopeTracker.isViewing(0)) {
+        if (MC.player != null && TelescopeTracker.isTelescope(MC.player.getOffhandItem()) && TelescopeTracker.isViewing(0)) {
             list.add(data.t1);
         } else {
-            list.add(mc.player != null && mc.player.isShiftKeyDown() ? data.h1 :data.c1);
+            list.add(MC.player != null && MC.player.isShiftKeyDown() ? data.h1 :data.c1);
         }
 
         list.forEach(p -> addAxes(poseStack, bufferbuilder, data, p));
@@ -115,14 +115,14 @@ public class DebugRenderHelper {
         PoseStack poseStack, BufferBuilder bufferbuilder, VRData data, VRData.VRDevicePose pose)
     {
         Vector3f position = pose.getPosition()
-            .subtract(RenderHelper.getSmoothCameraPosition(dataHolder.currentPass, data)).toVector3f();
+            .subtract(RenderHelper.getSmoothCameraPosition(DATA_HOLDER.currentPass, data)).toVector3f();
         Vector3f forward = pose.getDirection().mul(0.25F);
         Vector3f up = pose.getCustomVector(MathUtils.UP).mul(0.25F);
         Vector3f right = pose.getCustomVector(MathUtils.RIGHT).mul(0.25F);
 
-        addLine(poseStack, bufferbuilder, position, forward, blue);
-        addLine(poseStack, bufferbuilder, position, up, green);
-        addLine(poseStack, bufferbuilder, position, right, red);
+        addLine(poseStack, bufferbuilder, position, forward, BLUE);
+        addLine(poseStack, bufferbuilder, position, up, GREEN);
+        addLine(poseStack, bufferbuilder, position, right, RED);
     }
 
     private static void addAxes(
@@ -135,9 +135,9 @@ public class DebugRenderHelper {
         Vector3f up = rot.transform(MathUtils.UP, new Vector3f()).mul(0.25F);
         Vector3f right = rot.transform(MathUtils.RIGHT, new Vector3f()).mul(0.25F);
 
-        addLine(poseStack, bufferbuilder, position, forward, blue);
-        addLine(poseStack, bufferbuilder, position, up, green);
-        addLine(poseStack, bufferbuilder, position, right, red);
+        addLine(poseStack, bufferbuilder, position, forward, BLUE);
+        addLine(poseStack, bufferbuilder, position, up, GREEN);
+        addLine(poseStack, bufferbuilder, position, right, RED);
     }
 
     private static void addLine(PoseStack poseStack, BufferBuilder bufferbuilder, Vector3f position, Vector3f dir, Vector3f color) {
