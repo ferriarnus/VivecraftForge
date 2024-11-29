fayer3/VivecraftMod

move MathUtils to the common package rename the Utils in the client package to ClientUtils

common/src/main/java/org/vivecraft/client_vr/provider/InputSimulator.java
@@ -3,7 +3,7 @@ package org.vivecraft.client_vr.provider;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 import org.lwjgl.glfw.GLFW;
-import org.vivecraft.client.utils.Utils;
+import org.vivecraft.client.utils.ClientUtils;
 import org.vivecraft.client_vr.ClientDataHolderVR;
 
 import java.util.HashMap;
@@ -107,9 +107,9 @@ public class InputSimulator {
             if (code != GLFW.GLFW_KEY_UNKNOWN) {
                 pressKey(code);
             }
-        } else if (minecraft.screen == null && Utils.milliTime() - airTypingWarningTime >= 30000) {
+        } else if (minecraft.screen == null && ClientUtils.milliTime() - airTypingWarningTime >= 30000) {
             minecraft.gui.getChat().addMessage(Component.translatable("vivecraft.messages.airtypingwarning"));
-            airTypingWarningTime = Utils.milliTime();
+            airTypingWarningTime = ClientUtils.milliTime();
         }
     }
 

ferriarnus/VivecraftForge

common/src/main/java/org/vivecraft/client_vr/provider/InputSimulator.java
@@ -3,7 +3,7 @@ package org.vivecraft.client_vr.provider;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 import org.lwjgl.glfw.GLFW;
-import org.vivecraft.client.utils.Utils;
+import org.vivecraft.client.utils.ClientUtils;
 import org.vivecraft.client_vr.ClientDataHolderVR;
 
 import java.util.HashMap;
@@ -107,9 +107,9 @@ public class InputSimulator {
             if (code != GLFW.GLFW_KEY_UNKNOWN) {
                 pressKey(code);
             }
-        } else if (minecraft.screen == null && Utils.milliTime() - AIR_TYPING_WARNING_TIME >= 30000) {
+        } else if (minecraft.screen == null && ClientUtils.milliTime() - AIR_TYPING_WARNING_TIME >= 30000) {
             minecraft.gui.getChat().addMessage(Component.translatable("vivecraft.messages.airtypingwarning"));
-            AIR_TYPING_WARNING_TIME = Utils.milliTime();
+            AIR_TYPING_WARNING_TIME = ClientUtils.milliTime();
         }
     }
 