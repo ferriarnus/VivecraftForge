fayer3/VivecraftMod

mixin into PostChain, to make sub chains for each RenderPass, instead of managing them externally, fixes many mods that use custom PostChains

common/src/main/java/org/vivecraft/client_xr/render_pass/WorldRenderPass.java
@@ -2,14 +2,11 @@ package org.vivecraft.client_xr.render_pass;
 
 import com.mojang.blaze3d.pipeline.RenderTarget;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.PostChain;
-import net.minecraft.resources.ResourceLocation;
+import org.vivecraft.client_vr.render.RenderPass;
 
 import java.io.IOException;
 
 public class WorldRenderPass implements AutoCloseable {
-
-    private static final Minecraft mc = Minecraft.getInstance();
 
     public static WorldRenderPass stereoXR;
     public static WorldRenderPass center;
@@ -18,11 +15,7 @@ public class WorldRenderPass implements AutoCloseable {
     public static WorldRenderPass rightTelescope;
     public static WorldRenderPass camera;
 
-
     public final RenderTarget target;
-    public final PostChain transparencyChain;
-    public final PostChain outlineChain;
-    public PostChain postEffect = null;
 
     /**
      * creates a WorldRenderPass that writes to {@code target}
@@ -31,40 +24,30 @@ public class WorldRenderPass implements AutoCloseable {
      */
     public WorldRenderPass(RenderTarget target) throws IOException {
         this.target = target;
-        if (Minecraft.useShaderTransparency()) {
-            this.transparencyChain = createPostChain(new ResourceLocation("shaders/post/vrtransparency.json"), this.target);
-        } else {
-            this.transparencyChain = null;
-        }
-        this.outlineChain = createPostChain(new ResourceLocation("shaders/post/entity_outline.json"), this.target);
     }
 
     /**
-     * creates a post chain for the given shader, and resizes it to the {@code target} size
-     * @param resourceLocation shader to load
-     * @param target RenderTarget the shader should write to
-     * @throws IOException when an error occurs during shader loading
+     * @param pass RenderPass to get the WorldRenderPass for
+     * @return the WorldRenderPass object corresponding to the given {@code pass}
      */
-    public static PostChain createPostChain(ResourceLocation resourceLocation, RenderTarget target) throws IOException {
-        PostChain postchain = new PostChain(mc.getTextureManager(), mc.getResourceManager(), target, resourceLocation);
-        postchain.resize(target.viewWidth, target.viewHeight);
-        return postchain;
+    public static WorldRenderPass getByRenderPass(RenderPass pass) {
+        return switch (pass) {
+            case CENTER -> center;
+            case THIRD -> mixedReality;
+            case SCOPEL -> leftTelescope;
+            case SCOPER -> rightTelescope;
+            case CAMERA -> camera;
+            default -> stereoXR;
+        };
     }
 
     /**
-     * resizes the RenderTarget and the PostChains of this pass to the given size
+     * resizes the RenderTarget of this pass to the given size
      * @param width new width
      * @param height new height
      */
     public void resize(int width, int height) {
         this.target.resize(width, height, Minecraft.ON_OSX);
-        this.outlineChain.resize(width, height);
-        if (this.transparencyChain != null) {
-            this.transparencyChain.resize(width, height);
-        }
-        if (this.postEffect != null) {
-            this.postEffect.resize(width, height);
-        }
     }
 
     /**
@@ -73,13 +56,5 @@ public class WorldRenderPass implements AutoCloseable {
     @Override
     public void close() {
         this.target.destroyBuffers();
-        if (this.transparencyChain != null) {
-            this.transparencyChain.close();
-        }
-        this.outlineChain.close();
-        if (this.postEffect != null) {
-            this.postEffect.close();
-            this.postEffect = null;
-        }
     }
 }

common/src/main/java/org/vivecraft/mixin/client_vr/renderer/PostChainVRMixin.java
@@ -3,12 +3,30 @@ package org.vivecraft.mixin.client_vr.renderer;
 import com.mojang.blaze3d.pipeline.RenderTarget;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.PostChain;
+import net.minecraft.client.renderer.texture.TextureManager;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.vivecraft.client.extensions.RenderTargetExtension;
+import org.vivecraft.client_vr.ClientDataHolderVR;
+import org.vivecraft.client_vr.MultiPassRenderTarget;
+import org.vivecraft.client_vr.VRState;
+import org.vivecraft.client_vr.render.RenderPass;
+import org.vivecraft.client_xr.render_pass.RenderPassManager;
+import org.vivecraft.client_xr.render_pass.RenderPassType;
+import org.vivecraft.client_xr.render_pass.WorldRenderPass;
+
+import java.io.IOException;
+import java.util.EnumMap;
+import java.util.Map;
 
 @Mixin(PostChain.class)
 public class PostChainVRMixin {
@@ -17,6 +35,33 @@ public class PostChainVRMixin {
     @Final
     private RenderTarget screenTarget;
 
+    @Unique
+    private final EnumMap<RenderPass, PostChain> vivecraft$VRPostChains = new EnumMap<>(RenderPass.class);
+
+    @Inject(method = "<init>", at = @At(value = "TAIL"))
+    private void vivecraft$createVRChains(
+        TextureManager textureManager, ResourceManager resourceManager, RenderTarget screenTarget,
+        ResourceLocation name, CallbackInfo ci) throws IOException
+    {
+        if (VRState.vrInitialized && this.screenTarget == RenderPassManager.INSTANCE.vanillaRenderTarget) {
+            for (RenderPass pass : ClientDataHolderVR.getInstance().vrRenderer.getRenderPasses()) {
+                this.vivecraft$VRPostChains.put(pass,
+                    new PostChain(textureManager, resourceManager, WorldRenderPass.getByRenderPass(pass).target, name));
+            }
+        }
+    }
+
+    @Inject(method = "getTempTarget", at = @At("RETURN"), cancellable = true)
+    private void vivecraft$getVRTempTarget(String attributeName, CallbackInfoReturnable<RenderTarget> cir) {
+        if (VRState.vrInitialized && !this.vivecraft$VRPostChains.isEmpty()) {
+            EnumMap<RenderPass, RenderTarget> vrTargets = new EnumMap<>(RenderPass.class);
+            for (Map.Entry<RenderPass, PostChain> entry : this.vivecraft$VRPostChains.entrySet()) {
+                vrTargets.put(entry.getKey(), entry.getValue().getTempTarget(attributeName));
+            }
+            cir.setReturnValue(new MultiPassRenderTarget(cir.getReturnValue(), vrTargets));
+        }
+    }
+
     @ModifyVariable(method = "addTempTarget", at = @At(value = "STORE"), ordinal = 0)
     private RenderTarget vivecraft$vrTargetStencil(RenderTarget renderTarget) {
         if (((RenderTargetExtension) this.screenTarget).vivecraft$hasStencil()) {
@@ -24,5 +69,31 @@ public class PostChainVRMixin {
             renderTarget.resize(renderTarget.width, renderTarget.height, Minecraft.ON_OSX);
         }
         return renderTarget;
+    }
+
+    @Inject(method = "close", at = @At("TAIL"))
+    private void vivecraft$closeVRChains(CallbackInfo ci) {
+        for (PostChain postChain : this.vivecraft$VRPostChains.values()) {
+            postChain.close();
+        }
+        this.vivecraft$VRPostChains.clear();
+    }
+
+    @Inject(method = "resize", at = @At("TAIL"))
+    private void vivecraft$resizeVRChains(CallbackInfo ci) {
+        for (Map.Entry<RenderPass, PostChain> entry : this.vivecraft$VRPostChains.entrySet()) {
+            RenderTarget target = WorldRenderPass.getByRenderPass(entry.getKey()).target;
+            entry.getValue().resize(target.width, target.height);
+        }
+    }
+
+    @Inject(method = "process", at = @At(value = "HEAD"), cancellable = true)
+    private void vivecraft$renderVRChain(float partialTick, CallbackInfo ci) {
+        if (!RenderPassType.isVanilla() &&
+            this.vivecraft$VRPostChains.containsKey(ClientDataHolderVR.getInstance().currentPass))
+        {
+            this.vivecraft$VRPostChains.get(ClientDataHolderVR.getInstance().currentPass).process(partialTick);
+            ci.cancel();
+        }
     }
 }

ferriarnus/VivecraftForge

common/src/main/java/org/vivecraft/client_xr/render_pass/WorldRenderPass.java
@@ -2,14 +2,11 @@ package org.vivecraft.client_xr.render_pass;
 
 import com.mojang.blaze3d.pipeline.RenderTarget;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.PostChain;
-import net.minecraft.resources.ResourceLocation;
+import org.vivecraft.client_vr.render.RenderPass;
 
 import java.io.IOException;
 
 public class WorldRenderPass implements AutoCloseable {
-
-    private static final Minecraft MC = Minecraft.getInstance();
 
     public static WorldRenderPass STEREO_XR;
     public static WorldRenderPass CENTER;
@@ -18,11 +15,7 @@ public class WorldRenderPass implements AutoCloseable {
     public static WorldRenderPass RIGHT_TELESCOPE;
     public static WorldRenderPass CAMERA;
 
-
     public final RenderTarget target;
-    public final PostChain transparencyChain;
-    public final PostChain outlineChain;
-    public PostChain postEffect = null;
 
     /**
      * creates a WorldRenderPass that writes to {@code target}
@@ -31,40 +24,30 @@ public class WorldRenderPass implements AutoCloseable {
      */
     public WorldRenderPass(RenderTarget target) throws IOException {
         this.target = target;
-        if (Minecraft.useShaderTransparency()) {
-            this.transparencyChain = createPostChain(new ResourceLocation("shaders/post/vrtransparency.json"), this.target);
-        } else {
-            this.transparencyChain = null;
-        }
-        this.outlineChain = createPostChain(new ResourceLocation("shaders/post/entity_outline.json"), this.target);
     }
 
     /**
-     * creates a post chain for the given shader, and resizes it to the {@code target} size
-     * @param resourceLocation shader to load
-     * @param target RenderTarget the shader should write to
-     * @throws IOException when an error occurs during shader loading
+     * @param pass RenderPass to get the WorldRenderPass for
+     * @return the WorldRenderPass object corresponding to the given {@code pass}
      */
-    public static PostChain createPostChain(ResourceLocation resourceLocation, RenderTarget target) throws IOException {
-        PostChain postchain = new PostChain(MC.getTextureManager(), MC.getResourceManager(), target, resourceLocation);
-        postchain.resize(target.viewWidth, target.viewHeight);
-        return postchain;
+    public static WorldRenderPass getByRenderPass(RenderPass pass) {
+        return switch (pass) {
+            case CENTER -> CENTER;
+            case THIRD -> MIXED_REALITY;
+            case SCOPEL -> LEFT_TELESCOPE;
+            case SCOPER -> RIGHT_TELESCOPE;
+            case CAMERA -> CENTER;
+            default -> STEREO_XR;
+        };
     }
 
     /**
-     * resizes the RenderTarget and the PostChains of this pass to the given size
+     * resizes the RenderTarget of this pass to the given size
      * @param width new width
      * @param height new height
      */
     public void resize(int width, int height) {
         this.target.resize(width, height, Minecraft.ON_OSX);
-        this.outlineChain.resize(width, height);
-        if (this.transparencyChain != null) {
-            this.transparencyChain.resize(width, height);
-        }
-        if (this.postEffect != null) {
-            this.postEffect.resize(width, height);
-        }
     }
 
     /**
@@ -73,13 +56,5 @@ public class WorldRenderPass implements AutoCloseable {
     @Override
     public void close() {
         this.target.destroyBuffers();
-        if (this.transparencyChain != null) {
-            this.transparencyChain.close();
-        }
-        this.outlineChain.close();
-        if (this.postEffect != null) {
-            this.postEffect.close();
-            this.postEffect = null;
-        }
     }
 }

common/src/main/java/org/vivecraft/mixin/client_vr/renderer/PostChainVRMixin.java
@@ -3,12 +3,30 @@ package org.vivecraft.mixin.client_vr.renderer;
 import com.mojang.blaze3d.pipeline.RenderTarget;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.PostChain;
+import net.minecraft.client.renderer.texture.TextureManager;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.vivecraft.client.extensions.RenderTargetExtension;
+import org.vivecraft.client_vr.ClientDataHolderVR;
+import org.vivecraft.client_vr.MultiPassRenderTarget;
+import org.vivecraft.client_vr.VRState;
+import org.vivecraft.client_vr.render.RenderPass;
+import org.vivecraft.client_xr.render_pass.RenderPassManager;
+import org.vivecraft.client_xr.render_pass.RenderPassType;
+import org.vivecraft.client_xr.render_pass.WorldRenderPass;
+
+import java.io.IOException;
+import java.util.EnumMap;
+import java.util.Map;
 
 @Mixin(PostChain.class)
 public class PostChainVRMixin {
@@ -17,6 +35,33 @@ public class PostChainVRMixin {
     @Final
     private RenderTarget screenTarget;
 
+    @Unique
+    private final EnumMap<RenderPass, PostChain> vivecraft$VRPostChains = new EnumMap<>(RenderPass.class);
+
+    @Inject(method = "<init>", at = @At(value = "TAIL"))
+    private void vivecraft$createVRChains(
+        TextureManager textureManager, ResourceManager resourceManager, RenderTarget screenTarget,
+        ResourceLocation name, CallbackInfo ci) throws IOException
+    {
+        if (VRState.VR_INITIALIZED && this.screenTarget == RenderPassManager.INSTANCE.vanillaRenderTarget) {
+            for (RenderPass pass : ClientDataHolderVR.getInstance().vrRenderer.getRenderPasses()) {
+                this.vivecraft$VRPostChains.put(pass,
+                    new PostChain(textureManager, resourceManager, WorldRenderPass.getByRenderPass(pass).target, name));
+            }
+        }
+    }
+
+    @Inject(method = "getTempTarget", at = @At("RETURN"), cancellable = true)
+    private void vivecraft$getVRTempTarget(String attributeName, CallbackInfoReturnable<RenderTarget> cir) {
+        if (VRState.VR_INITIALIZED && !this.vivecraft$VRPostChains.isEmpty()) {
+            EnumMap<RenderPass, RenderTarget> vrTargets = new EnumMap<>(RenderPass.class);
+            for (Map.Entry<RenderPass, PostChain> entry : this.vivecraft$VRPostChains.entrySet()) {
+                vrTargets.put(entry.getKey(), entry.getValue().getTempTarget(attributeName));
+            }
+            cir.setReturnValue(new MultiPassRenderTarget(cir.getReturnValue(), vrTargets));
+        }
+    }
+
     @ModifyVariable(method = "addTempTarget", at = @At(value = "STORE"), ordinal = 0)
     private RenderTarget vivecraft$vrTargetStencil(RenderTarget renderTarget) {
         if (((RenderTargetExtension) this.screenTarget).vivecraft$hasStencil()) {
@@ -24,5 +69,31 @@ public class PostChainVRMixin {
             renderTarget.resize(renderTarget.width, renderTarget.height, Minecraft.ON_OSX);
         }
         return renderTarget;
+    }
+
+    @Inject(method = "close", at = @At("TAIL"))
+    private void vivecraft$closeVRChains(CallbackInfo ci) {
+        for (PostChain postChain : this.vivecraft$VRPostChains.values()) {
+            postChain.close();
+        }
+        this.vivecraft$VRPostChains.clear();
+    }
+
+    @Inject(method = "resize", at = @At("TAIL"))
+    private void vivecraft$resizeVRChains(CallbackInfo ci) {
+        for (Map.Entry<RenderPass, PostChain> entry : this.vivecraft$VRPostChains.entrySet()) {
+            RenderTarget target = WorldRenderPass.getByRenderPass(entry.getKey()).target;
+            entry.getValue().resize(target.width, target.height);
+        }
+    }
+
+    @Inject(method = "process", at = @At(value = "HEAD"), cancellable = true)
+    private void vivecraft$renderVRChain(float partialTick, CallbackInfo ci) {
+        if (!RenderPassType.isVanilla() &&
+            this.vivecraft$VRPostChains.containsKey(ClientDataHolderVR.getInstance().currentPass))
+        {
+            this.vivecraft$VRPostChains.get(ClientDataHolderVR.getInstance().currentPass).process(partialTick);
+            ci.cancel();
+        }
     }
 }