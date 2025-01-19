Vivecraft/VivecraftMod

Add integration for Origins mod climbing power

common/src/main/java/org/vivecraft/client_vr/render/helpers/VRArmHelper.java
@@ -17,6 +17,7 @@ import org.lwjgl.opengl.GL11C;
 import org.vivecraft.client.network.ClientNetworking;
 import org.vivecraft.client_vr.ClientDataHolderVR;
 import org.vivecraft.client_vr.extensions.GameRendererExtension;
+import org.vivecraft.client_vr.extensions.PlayerExtension;
 import org.vivecraft.client_vr.gameplay.trackers.BowTracker;
 import org.vivecraft.client_vr.render.RenderPass;
 import org.vivecraft.client_vr.settings.VRSettings;
@@ -142,7 +143,8 @@ public class VRArmHelper {
             item = override;
         }
 
-        if (dataHolder.climbTracker.isClimbeyClimb() && !dataHolder.climbTracker.isClaws(item) && override == null) {
+        if (dataHolder.climbTracker.isClimbeyClimb() && ((PlayerExtension) mc.player).vivecraft$isClimbeyClimbEquipped() &&
+            !dataHolder.climbTracker.isClaws(item) && override == null) {
             item = mc.player.getOffhandItem();
         }
 
@@ -205,7 +207,8 @@ public class VRArmHelper {
             item = override;
         }
 
-        if (dataHolder.climbTracker.isClimbeyClimb() && !dataHolder.climbTracker.isClaws(item) && override == null) {
+        if (dataHolder.climbTracker.isClimbeyClimb() && ((PlayerExtension) mc.player).vivecraft$isClimbeyClimbEquipped() &&
+            !dataHolder.climbTracker.isClaws(item) && override == null) {
             item = mc.player.getMainHandItem();
         }
 

ferriarnus/VivecraftForge

common/src/main/java/org/vivecraft/client_vr/render/helpers/VRArmHelper.java
@@ -178,8 +178,8 @@ public class VRArmHelper {
             item = override;
         }
 
-        // climbey override
-        if (DATA_HOLDER.climbTracker.isClimbeyClimb() && !ClimbTracker.isClaws(item) && override == null) {
+        if (DATA_HOLDER.climbTracker.isClimbeyClimb() && ClimbTracker.hasClimbeyClimbEquipped(MC.player) &&
+            !DATA_HOLDER.climbTracker.isClaws(item) && override == null) {
             item = MC.player.getOffhandItem();
         }
 
@@ -233,8 +233,8 @@ public class VRArmHelper {
                 item = override;
             }
 
-            // climbey override
-            if (DATA_HOLDER.climbTracker.isClimbeyClimb() && !ClimbTracker.isClaws(item) && override == null) {
+            if (DATA_HOLDER.climbTracker.isClimbeyClimb() && ClimbTracker.hasClimbeyClimbEquipped(MC.player) &&
+                !DATA_HOLDER.climbTracker.isClaws(item) && override == null) {
                 item = MC.player.getMainHandItem();
             }
 